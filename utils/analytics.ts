4f0a1af6a6c354eed810168770835fe4d151e62a
Anas Almutary <me@ianas.me>
2025-03-04 03:39:59 +0300

    update analytics

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,15 +1,17 @@
+import axios from "axios";
+
 const analytics = {
     getAnalytics: async (shortUrl: string) => {
         try {
-            const response = await fetch(`/api/analytics?shortUrl=${encodeURIComponent(shortUrl)}`);
+            const response = await axios.get(`/api/analytics?shortUrl=${encodeURIComponent(shortUrl)}`);
 
-            if (response.ok) {
-                return response.json();
+            if (response.status === 200) {
+                return response.data;
             }
 
             throw new Error('Error while fetching analytics');
         } catch (error: any) {
-            return { error: error.response }
+            return error.response.data;
         }
     }
 }
--- utils/shorten.ts
+++ utils/shorten.ts
@@ -1,34 +1,30 @@
+import axios from "axios";
+
 const shorten = {
     get: async (shortUrl: string) => {
         try {
-            const response = await fetch(`/api/shorten?shortUrl=${encodeURIComponent(shortUrl)}`);
+            const response = await axios.get(`/api/shorten?shortUrl=${encodeURIComponent(shortUrl)}`);
 
-            if (response.ok) {
-                return response.json();
+            if (response.status === 200) {
+                return response.data;
             }
 
             throw new Error('Error while fetching shortened URL');
         } catch (error: any) {
-            return { error: error.response }
+            return error.response.data;
         }
     },
     create: async (originalUrl: string) => {
         try {
-            const response = await fetch(`/api/shorten`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ originalUrl}),
-            });
+            const response = await axios.post(`/api/shorten`, { originalUrl });
 
-            if (response.ok) {
-                return response.json();
+            if (response.status === 201) {
+                return response.data;
             }
 
             throw new Error('Error while creating shortened URL');
         } catch (error: any) {
-            return { error: error.response };
+            return error.response.data;
         }
     }
 }
