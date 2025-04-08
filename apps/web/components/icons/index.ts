1783d77b25da75ec51727bfc86a43c63b7ab138a
Anas Almutary <me@ianas.me>
2025-04-09 00:55:11 +0300

    add not found page

--- apps/web/components/icons/index.ts
+++ apps/web/components/icons/index.ts
@@ -1,5 +1,6 @@
 import Github from "./github";
 import Document from "./document";
 import { Sun, Moon } from "./theme";
+import Error404 from "./Error404";
 
-export { Github, Document, Sun, Moon };
+export { Github, Document, Sun, Moon, Error404 };
