085a16911a9f66b56e5f7aad034ddd3ccc38aec5
Anas Almutary <me@ianas.me>
2025-04-08 23:47:16 +0300

    add theme provider

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import ThemeProvider from "@/providers/ThemeProvider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -35,9 +36,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        {children}
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
